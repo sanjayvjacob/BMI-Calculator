ca5f33e882a2e35e59138019c046a015e9eaa019
Sanjay V Jacob <76661259+sanjayvjacob@users.noreply.github.com>
2023-12-24 21:30:23 +0000

    Add if else statement

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function bmiCalculator(weight, height){
   var bmi = weight / (height * height);
-  return Math.round(bmi);
+  if (bmi < 18.5) {
+      return "Your BMI is " + Math.round(bmi) + ", so you are underweight.";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return "Your BMI is " + Math.round(bmi) + ", so you have a normal weight.";
+  } else {
+      return "Your BMI is " + Math.round(bmi) + ", so you are overweight.";
+  }
 }
-var bmi = bmiCalculator(65, 1.8);
+var bmi = bmiCalculator(90, 1.8);
 console.log(bmi);
